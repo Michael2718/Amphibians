1ff53e063415a39bf1c5d9cf71e43fd089f74c0b
Michael <isaevmihail007@gmail.com>
2023-08-10 19:41:53 +0300

    Add ViewModel and UiState

--- app/src/main/java/com/example/amphibians/ui/AmphibiansUiState.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansUiState.kt
@@ -1,4 +1,5 @@
 package com.example.amphibians.ui
 
 class AmphibiansUiState {
+//    val requestStatus: RequestStatus
 }
--- app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansViewModel.kt
@@ -1,2 +1,24 @@
 package com.example.amphibians.ui
 
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+//sealed interface RequestStatus {
+//    data class Success(val amphibians: List<Amphibia>) : RequestStatus
+//    object Error : RequestStatus
+//    object Loading : RequestStatus
+//}
+
+class AmphibiansViewModel(
+//    private val amphibiansRepository: AmphibiansRepository
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(
+        AmphibiansUiState()
+    )
+    val uiState: StateFlow<AmphibiansUiState> = _uiState
+
+    init {
+        // Get Amphibians
+    }
+}
