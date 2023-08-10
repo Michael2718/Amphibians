1a93b7c189f23636a920650888d139e3dc425792
Michael <isaevmihail007@gmail.com>
2023-08-10 19:41:36 +0300

    Basic UI

--- app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -1,2 +1,65 @@
 package com.example.amphibians.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
+import com.example.amphibians.R
+import com.example.amphibians.ui.screens.HomeScreen
+
+
+@Composable
+fun AmphibiansApp(
+    modifier: Modifier = Modifier,
+) {
+    Scaffold(
+        topBar = {
+            AmphibiansTopAppBar(
+                canNavigateBack = false,
+                navigateUp = {}
+            )
+        }
+    ) { innerPadding ->
+        HomeScreen(
+            modifier = modifier.padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansTopAppBar(
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,2 +1,172 @@
 package com.example.amphibians.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.amphibians.R
+import com.example.amphibians.model.Amphibian
+import com.example.amphibians.ui.theme.AmphibiansTheme
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    // when (UiState)
+    AmphibiansList(
+        amphibians = listOf(
+            Amphibian(
+                name = "Great Basin Spadefoot",
+                type = "Toad",
+                description = "This toad spends most of its life underground due to the arid desert conditions in which it lives. Spadefoot toads earn the name because of their hind legs which are wedged to aid in digging. They are typically grey, green, or brown with dark spots.",
+                imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+            ),
+            Amphibian(
+                name = "Roraima Bush Toad",
+                type = "Toad",
+                description = "This toad is typically found in South America. Specifically on Mount Roraima at the boarders of Venezuala, Brazil, and Guyana, hence the name. The Roraiam Bush Toad is typically black with yellow spots or marbling along the throat and belly.",
+                imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/roraima-bush-toad.png"
+            )
+        ),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun AmphibiansList(
+    amphibians: List<Amphibian>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(items = amphibians, key = { amphibia -> amphibia.name }) { amphibia ->
+            AmphibiaCard(
+                amphibian = amphibia,
+                {},
+                modifier = Modifier
+                    .padding(4.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiaCard(
+    amphibian: Amphibian,
+    onItemClick: (Amphibian) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        onClick = { onItemClick(amphibian) },
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(
+//            modifier = Modifier.padding(4.dp)
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val title = "${amphibian.name} (${amphibian.type})"
+            Text(
+                text = title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_medium)),
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.titleLarge
+            )
+//            Box {
+//                AsyncImage(
+//                    model = ImageRequest.Builder(context = LocalContext.current)
+//                        .data(amphibia.imgSrc)
+//                        .crossfade(true)
+//                        .build(),
+//                    contentDescription = amphibia.name,
+//                    modifier = Modifier.fillMaxWidth(),
+//                    placeholder = painterResource(R.drawable.loading_img),
+//                    error = painterResource(R.drawable.ic_broken_image),
+//                    contentScale = ContentScale.Fit
+//                )
+//            }
+            Image(
+                painter = painterResource(R.drawable.great_basin_spadefoot),
+                contentDescription = amphibian.name,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = amphibian.description,
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+                textAlign = TextAlign.Justify,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AmphibiansListPreview() {
+    AmphibiansTheme {
+        AmphibiansList(
+            amphibians = listOf(
+                Amphibian(
+                    name = "Great Basin Spadefoot",
+                    type = "Toad",
+                    description = "This toad spends most of its life underground due to the arid desert conditions in which it lives. Spadefoot toads earn the name because of their hind legs which are wedged to aid in digging. They are typically grey, green, or brown with dark spots.",
+                    imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/great-basin-spadefoot.png"
+                ),
+                Amphibian(
+                    name = "Roraima Bush Toad",
+                    type = "Toad",
+                    description = "This toad is typically found in South America. Specifically on Mount Roraima at the boarders of Venezuala, Brazil, and Guyana, hence the name. The Roraiam Bush Toad is typically black with yellow spots or marbling along the throat and belly.",
+                    imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/roraima-bush-toad.png"
+                )
+            )
+        )
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun AmphibiaCardPreview() {
+//    AmphibiansTheme {
+//        val toad = Amphibia(
+//            name = "Great Basin Spadefoot",
+//            type = "Toad",
+//            description = "This toad is typically found in South America. Specifically on Mount Roraima at the boarders of Venezuala, Brazil, and Guyana, hence the name. The Roraiam Bush Toad is typically black with yellow spots or marbling along the throat and belly.",
+//            imgSrc = "https://developer.android.com/codelabs/basic-android-kotlin-compose-amphibians-app/img/roraima-bush-toad.png"
+//        )
+//        AmphibiaCard(
+//            amphibia = toad,
+//            {}
+//        )
+//    }
+//}
